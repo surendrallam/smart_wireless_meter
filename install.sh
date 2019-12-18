817242bc72612ae969d54e0017cccef6a9fbfe5a
Surendra Allam <surendrallam@hotmail.com>
2019-12-18 14:22:00 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
-sudo apt-get update
-sudo apt-get upgrade
+cd Desktop/
+wget https://raw.githubusercontent.com/surendrallam/smart_wireless_meter/master/twofish/sources.list
+sudo cp -r Desktop/source.list /etc/apt/sources.list
+
+
